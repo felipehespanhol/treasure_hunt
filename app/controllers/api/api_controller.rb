a36100f213899c1a944e11e18062489b739deb8f
Felipe G. Hespanhol <felipeghespanhol@gmail.com>
2024-04-19 15:44:37 -0300

    Secure the API with an API token

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,14 @@
 class Api::ApiController < ApplicationController
   # This is a base class for all API controllers. It provides some common
   # functionality that all API controllers will need.
+
+  before_action :authenticate!
+
+  private
+
+  def authenticate!
+    return if request.headers['HTTP_API_KEY'] == Rails.application.credentials.api_key
+
+    head :unauthorized
+  end
 end
--- spec/requests/api/winners_spec.rb
+++ spec/requests/api/winners_spec.rb
@@ -2,34 +2,48 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Winners', type: :request do
   describe 'GET /api/winners' do
-    let!(:best_guesses) do
-      1.upto(20).map do
-        user = create(:user)
+    let(:api_key) { Random.hex }
 
-        best_guess = create(:guess, user:, is_successful: true, distance: Random.rand(1..10))
-        create(:guess, user:, is_successful: true, distance: Random.rand(11..20))
-        create(:guess, user:, is_successful: false, distance: Random.rand(1001..10_000))
+    before { allow(Rails.application.credentials).to receive(:api_key).and_return(api_key) }
 
-        { id: user.id, email: user.email, best_distance: best_guess.distance }
-      end.sort_by { |o| o[:best_distance] }
+    context 'when the request does not have a valid API_KEY in its header' do
+      it 'returns a 401 status code' do
+        get '/api/winners', headers: { HTTP_API_KEY: 'invalid' }
+
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
 
-    it 'returns a list of users with their best guesses' do
-      get '/api/winners'
+    context 'when the request has a valid API_KEY in its header' do
+      let!(:best_guesses) do
+        1.upto(20).map do
+          user = create(:user)
 
-      expect(response.body).to eq({ winners: best_guesses.first(10) }.to_json)
-    end
+          best_guess = create(:guess, user:, is_successful: true, distance: Random.rand(1..10))
+          create(:guess, user:, is_successful: true, distance: Random.rand(11..20))
+          create(:guess, user:, is_successful: false, distance: Random.rand(1001..10_000))
 
-    it 'paginates the results' do
-      get '/api/winners', params: { page: 2 }
+          { id: user.id, email: user.email, best_distance: best_guess.distance }
+        end.sort_by { |o| o[:best_distance] }
+      end
 
-      expect(response.body).to eq({ winners: best_guesses[10..19] }.to_json)
-    end
+      it 'returns a list of users with their best guesses' do
+        get '/api/winners', headers: { HTTP_API_KEY: api_key }
 
-    it 'orders the results' do
-      get '/api/winners', params: { order: 'desc' }
+        expect(response.body).to eq({ winners: best_guesses.first(10) }.to_json)
+      end
 
-      expect(response.body).to eq({ winners: best_guesses.reverse.first(10) }.to_json)
+      it 'paginates the results' do
+        get '/api/winners', headers: { HTTP_API_KEY: api_key }, params: { page: 2 }
+
+        expect(response.body).to eq({ winners: best_guesses[10..19] }.to_json)
+      end
+
+      it 'orders the results' do
+        get '/api/winners', headers: { HTTP_API_KEY: api_key }, params: { order: 'desc' }
+
+        expect(response.body).to eq({ winners: best_guesses.reverse.first(10) }.to_json)
+      end
     end
   end
 end
